fc21885098f69abd5528fd7657c47ea1c5df5640
Ramozjikevic <bro.brobro@mail.ru>
2020-10-04 20:58:41 +0500

    fix naming

--- app/src/main/java/ru/ramozjikevic/mymap/di/module/ActivityModule.kt
+++ app/src/main/java/ru/ramozjikevic/mymap/di/module/ActivityModule.kt
@@ -2,11 +2,11 @@ package ru.ramozjikevic.mymap.di.module
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import ru.ramozjikevic.mymap.MainActivity
+import ru.ramozjikevic.mymap.ui.component.RootActivity
 
 @Module
 abstract class ActivityModule {
 
     @ContributesAndroidInjector()
-    abstract fun contributeMainActivity() : MainActivity
+    abstract fun contributeRootActivity() : RootActivity
 }
