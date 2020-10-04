55961af9dece75b8139ec92e7177a4d942848d2b
Ramozjikevic <bro.brobro@mail.ru>
2020-10-04 21:03:27 +0500

    refactoring

--- app/src/main/java/ru/ramozjikevic/mymap/domain/interactors/TestInteractor.kt
+++ app/src/main/java/ru/ramozjikevic/mymap/domain/interactors/TestInteractor.kt
@@ -1,13 +1,14 @@
 package ru.ramozjikevic.mymap.domain.interactors
 
-import ru.ramozjikevic.mymap.data.repository.TestRepositoryImpl
+import ru.ramozjikevic.mymap.data.repository.TestRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
-/*
+
 @Singleton
 class TestInteractor @Inject constructor(
-    private val testRepositoryImpl: TestRepositoryImpl)
-{
-    fun getTestData() = testRepositoryImpl.loadTestData()
-}*/
+    private val testRepository: TestRepository
+) {
+    fun loadNewData() = testRepository.saveTestData()
+    fun getTestData() = testRepository.loadTestData()
+}
