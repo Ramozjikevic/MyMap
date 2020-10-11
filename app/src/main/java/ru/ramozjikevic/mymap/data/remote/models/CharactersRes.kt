7009f5887cbda40a7d922d774cc9c430ddd0c3bd
Ramozjikevic <bro.brobro@mail.ru>
2020-10-11 23:25:26 +0500

    add new field

--- app/src/main/java/ru/ramozjikevic/mymap/data/remote/models/CharactersRes.kt
+++ app/src/main/java/ru/ramozjikevic/mymap/data/remote/models/CharactersRes.kt
@@ -2,6 +2,8 @@ package ru.ramozjikevic.mymap.data.remote.models
 
 data class CharactersRes(
     val url: String,
+    val father: String,
+    val mother: String,
     val name: String,
-    val gender: String
+    val gender: String,
 )
